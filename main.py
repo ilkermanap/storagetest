1323f9e291a0a1d0ee689e30c99bc35750a483ec
Ilker Manap <ilkermanap@gmail.com>
2024-10-30 13:15:44 +0100

    Saving fio output also with text format

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from storagetest import Storage, sample
 
 stest = Storage(testname="deneme", path="/tmp")
-
-stest.parse(sample)
-
-
-#stest.test(size="10MB", bs="16k")
+stest.test(size="10MB", bs="16k")
